03c901416cfb6fa725e3102e03da9c3af169cfe0
esmahaber <l1621012063@stud.sdu.edu.tr>
2019-10-13 17:36:39 +0300

    /api/directors/:director_id [DELETE]

--- routes/director.js
+++ routes/director.js
@@ -114,4 +114,36 @@ router.get('/', (req, res) => {
   });
 });
 
+router.put('/:director_id', (req, res, next) => {
+    const promise = Director.findByIdAndUpdate(
+        req.params.director_id,
+        req.body,
+        {
+            new: true
+        }
+    );
+
+    promise.then((director) => {
+        if (!director)
+            next({message: 'The director was not found', code: 444});
+
+        res.json(director);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.delete('/:director_id', (req, res, next) => {
+  const promise = Director.findByIdAndRemove(req.params.director_id);
+
+  promise.then((director) => {
+    if (!director)
+      next({message: 'The movie was not found', code: 444});
+
+    res.json({ status: 1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
 module.exports = router;
