6802c0af7e43d26cb7931bf6a5bb3962bcc04caa
esmahaber <l1621012063@stud.sdu.edu.tr>
2019-10-13 18:18:08 +0300

    movie validations

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,29 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required:  [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır '],
+        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    country: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    year: {
+        type: Number,
+        max: [2020, 'Geçersiz yıl girdiniz (`{MAX}`) yılından küçük olmalıdır']
+    },
+    imdb_score:  {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     date: {
         type: Date,
         default: Date.now
