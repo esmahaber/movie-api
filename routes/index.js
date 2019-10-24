4895b74d0693790823bc8aed9b276a2b21013a40
esmahaber <l1621012063@stud.sdu.edu.tr>
2019-10-24 14:01:07 +0300

    register [POST]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+
+
+//Models
+const User = require('../models/User');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next) => {
+  const { username, password } = req.body;
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    const user = new User({
+      username,
+      password: hash
+    });
+
+    const promise = user.save();
+    promise.then((data)=> {
+      res.json(data)
+    }).catch((err) => {
+      res.json(err);
+    })
+  });
+
 });
 
 module.exports = router;
